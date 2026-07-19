3c1605a8c4b66e1665b4e0f4d59656de84a7669a
salman <240262@tkmce.ac.in>
2026-07-19 20:41:55 +0530

    Configure application timezone

--- src/main/java/com/parking/backend/BackendApplication.java
+++ src/main/java/com/parking/backend/BackendApplication.java
@@ -1,18 +1,21 @@
 package com.parking.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.EnableAsync;
-
 
 @EnableScheduling
 @EnableAsync
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
